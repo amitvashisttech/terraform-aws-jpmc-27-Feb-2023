21a642209201f02e2ac95eb54ca1522d2396c8c2
Amit Vashist <amitvashist7@outlook.com>
2023-03-06 10:33:30 +0000

    23-Software-Provisiong-with-Ansible

---
+++ 24-EBS/variable.tf
@@ -0,0 +1,60 @@
+data "aws_ami" "myami" {
+  most_recent = true
+  owners  = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+locals { 
+  default_name = "${join("-", tolist([terraform.workspace, "example"]))}"
+}
+
+
+variable "region" {
+  default = "us-east-1"
+}
+
+
+variable "instance_type" { 
+  type = map
+  default = { 
+      default = "t2.nano"
+      dev     = "t2.micro"
+      prod    = "t2.micro"
+      mindtree = "x1.large"
+  }
+
+}
+
+
+
+variable "instance_count" { 
+  type = map
+  default = { 
+      default = "1"
+      dev     = "2"
+      prod    = "5"
+      mindtree = "7"
+  }
+
+}
+
+variable "key_name" { 
+  default = "terraform-key-27-Feb-2023"
+}
+
+variable "pvt_key" { 
+  default = "/root/.ssh/amitvashist-terraform.pem"
+}
+
+variable "sg_id" {
+  default = "sg-03a98777c184aed2a"
+}
+
+
+variable "zone" {
+  default = "us-east-1a"
+}
